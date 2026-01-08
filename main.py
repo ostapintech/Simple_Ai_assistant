b5f446aa53cf25b35f495fd314f1c810d8ff4fbd
ostapintech <ostapintech@gmail.com>
2026-01-08 18:41:53 +0200

    add fastAPI + early version frontend

--- main.py
+++ main.py
@@ -1,40 +1,55 @@
-# from fastapi import FastAPI
-# import uvicorn
-from langchain_core.messages import AIMessage
+from fastapi import FastAPI
+from pydantic import BaseModel
 from langchain.chat_models import init_chat_model
-from langchain.messages import HumanMessage, SystemMessage
 from dotenv import load_dotenv
-
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
 
 load_dotenv()
-def text_processing(prompt):
-    try:
-        gpt = init_chat_model(model = 'groq:openai/gpt-oss-20b')
+app = FastAPI(title='AI assistant')
 
+# Для локального тесту
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+class AIRequest(BaseModel):
+    text: str
+
+@app.post('/api/process')
+def text_processing(req: AIRequest):
+    prompt = req.text
+    try:
+        gpt = init_chat_model(model='groq:openai/gpt-oss-20b')
         messages = [
             {'role': 'system', 'content': 'You are a helpful assistant that summarize text in Ukrainian'},
-            {'role': 'user', 'content': ''}
+            {'role': 'user', 'content': prompt}
         ]
-
-        messages[1].update({'content': prompt})
         summarize = gpt.invoke(messages)
 
-        llama = init_chat_model(model = 'groq:llama-3.1-8b-instant')
-
+        llama = init_chat_model(model='groq:llama-3.1-8b-instant')
         messages = [
             {'role': 'system', 'content': 'You are a translator from Ukrainian to English'},
-            {'role': 'user', 'content': f'{summarize.content}'}
-        ]
+            {'role': 'user', 'content': f"""
+                INSTRUCTIONS:
+                - Translate clearly given text
+                - Output only translated text
 
+                TEXT:
+                {summarize.content}"""}
+        ]
         translate = llama.invoke(messages)
-        return translate.content
+
+        return ({'summary': summarize.content, 'translation': translate.content})
+
 
     except Exception as e:
-        print(f'Виникла проблема з API: {e}')
+            print({'error': f'Виникла проблема з API: {e}'})
+
 
 if __name__ == '__main__':
-    prompt = input("Введіть текст для обробки: ")
-    if prompt.strip():
-        print(text_processing(prompt))
-    else:
-        print("Введіть текст")
+    import uvicorn
+    uvicorn.run("main:app", reload=True, host='0.0.0.0', port=8000)
